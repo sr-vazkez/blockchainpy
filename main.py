97aa9d1997c4f52eb99df9289679de63ba0d48c2
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-05-19 13:09:46 -0500

    Escribiendo funcion para construir nuevos bloques

--- main.py
+++ main.py
@@ -12,4 +12,19 @@ class Blockchain(object):
         self.pending_transactions = []
 
         self.new_block(
-            previous_hashs="El tiempo 03/Ene/2009 Canciller al borde de un segundo rescate bancario.", proof=100)
+            previous_hash="El tiempo 03/Ene/2009 Canciller al borde de un segundo rescate bancario.", proof=100)
+
+     def new_block(self, proof, previous_hash=None):
+          block = {
+               'index': len(self.chain) + 1,
+               'timestamp': time(),
+               'transactions': self.pending_transactions,
+               'proof': proof,
+               'previous_hash': previous_hash or self.hash(self.chain[-1]) 
+          }
+         self.pending_transactions = []
+         self.chain.append(block)
+         
+         
+         return block
+    
