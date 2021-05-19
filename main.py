62912e2b40bc39baf30cce8fe67d67053d791be2
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-05-19 12:55:18 -0500

    Finalizando funcion blockchain

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 import hashlib
 import json
 from time import time
+
+# Creando Clase BlockChain
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+
+        self.new_block(
+            previous_hashs="El tiempo 03/Ene/2009 Canciller al borde de un segundo rescate bancario.", proof=100)
